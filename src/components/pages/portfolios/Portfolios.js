02bbabdb2d0004dc092d07ff86e73ce8ee13d01c
steven ko <stevenko1213@gmail.com>
2023-09-06 19:19:59 +0800

    add portfolios

--- src/components/pages/portfolios/Portfolios.js
+++ src/components/pages/portfolios/Portfolios.js
@@ -1,13 +1,47 @@
 import React from 'react';
 
-export default function Portfolios() {
-  return <div>
+function Portfolios() {
+  return (
+
     <div className="card">
+
       <div className="card-content">
-        <h6>
-          <strong>Portfolios</strong>
-        </h6>
+        <div className="card-body">
+          <h6>
+            <strong>PORTFOLIOS</strong>
+          </h6>
+          <div className="mt-5">
+            <h6 className="card-title"><strong>1. Note</strong></h6>
+            <a href={"https://stevenkonote.netlify.app/"} className="btn btn-primary mt-top">Visit Website</a>
+          </div>
+          <div className="mt-5">
+            <h6 className="card-title"><strong>2. Contacts</strong></h6>
+            <a href={"https://app.netlify.com/sites/stevenkocontacts/overview"} className="btn btn-primary mt-top">Visit Website</a>
+          </div>
+          <div className="mt-5">
+            <h6 className="card-title"><strong>3. Todolist</strong></h6>
+            <a href={"https://app.netlify.com/sites/stevenkotodolist/overview"} className="btn btn-primary mt-top">Visit Website</a>
+          </div>
+          <div className="mt-5">
+            <h6 className="card-title"><strong>4. quiz</strong></h6>
+            <a href={"https://app.netlify.com/sites/stevenkoquiz/overview"} className="btn btn-primary mt-top">Visit Website</a>
+          </div>
+          <div className="mt-5">
+            <h6 className="card-title"><strong>5. password</strong></h6>
+            <a href={"https://app.netlify.com/sites/stevenkopassword/overview"} className="btn btn-primary mt-top">Visit Website</a>
+          </div>
+          <div className="mt-5">
+            <h6 className="card-title"><strong>6. ecommence</strong></h6>
+            <a href={"https://app.netlify.com/sites/stevenkoecommence/overview"} className="btn btn-primary mt-top">Visit Website</a>
+          </div>
+          <div className="mt-5">
+            <h6 className="card-title"><strong>7. minigame</strong></h6>
+            <a href={"https://app.netlify.com/sites/stevenkominigame/overview"} className="btn btn-primary mt-top">Visit Website</a>
+          </div>
+        </div>
       </div>
     </div>
-  </div>;
+  );
 }
+
+export default Portfolios;
